ec31646abf26083e9cbddd4647236b8e4b247ab7
shivi96 <shivigupta2628@gmail.com>
2018-05-04 19:25:29 +0530

    new commit pattern

---
+++ pattern1.java
@@ -0,0 +1,14 @@
+class pattern1{
+public static void main(String[] args){
+for(int j=1;j<7;j++){
+ for(int i=0; i<=6;i++){
+ if (i==0 || i==6-j){
+System.out.print(j);
+}
+System.out.print(" ");
+}
+System.out.println();
+}
+}
+}
+
